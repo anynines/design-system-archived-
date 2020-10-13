feb07523860a68adca0a7215feef537c3f13901b
anynines-johannchopin <jchopin@anynines.com>
2020-10-13 13:51:36 +0200

    🎨 Fix indentation

--- src/components/molecules/MenuItem/MenuItem.tsx
+++ src/components/molecules/MenuItem/MenuItem.tsx
@@ -43,9 +43,7 @@ export const MenuItem: React.FC<MenuItemProps> = ({
             {icon && <Icon icon={icon} />}
             {children !== undefined ? (
               <span className='label'>{children}</span>
-            ) : (
-              <></>
-            )}
+            ) : <></>}
           </NavLink>
         </StyledMenuItem>
       )}
@@ -65,8 +63,8 @@ export const MenuItem: React.FC<MenuItemProps> = ({
             {children !== undefined ? (
               <span className='label'>{children}</span>
             ) : (
-              <></>
-            )}
+                <></>
+              )}
           </div>
         </StyledMenuItem>
       )}
