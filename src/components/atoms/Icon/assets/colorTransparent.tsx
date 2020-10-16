ca5c00bac67b086434ca14353eca8939f947ba35
Sven Reiser <sreiser@anynines.com>
2020-10-16 13:07:19 +0200

    updates icons

--- src/components/atoms/Icon/assets/colorTransparent.tsx
+++ src/components/atoms/Icon/assets/colorTransparent.tsx
@@ -6,10 +6,10 @@ const colorTransparent = () => {
       <g id='Gruppe_3052' data-name='Gruppe 3052' transform='translate(-1678.723 -729.651)'>
         <g id='drop' transform='translate(1645.496 729.65)'>
           <g id='Gruppe_1021' data-name='Gruppe 1021' transform='translate(35.275 0)'>
-            <path id='Pfad_471' data-name='Pfad 471' d='M42.01.581c-.462-.788-.951-.76-1.412,0-2.118,3.15-5.323,7.876-5.323,10.239a6.029,6.029,0,1,0,12.059,0C47.334,8.43,44.129,3.731,42.01.581ZM45.65,13.508a5.157,5.157,0,0,1-1.82,1.765A.729.729,0,0,1,43.124,14a3.6,3.6,0,0,0,1.3-1.249,3.54,3.54,0,0,0,.543-1.738.721.721,0,1,1,1.439.081A5.38,5.38,0,0,1,45.65,13.508Z' transform='translate(-35.275 0)' fill='#fff' />
+            <path id='Pfad_471' data-name='Pfad 471' d='M42.01.581c-.462-.788-.951-.76-1.412,0-2.118,3.15-5.323,7.876-5.323,10.239a6.029,6.029,0,1,0,12.059,0C47.334,8.43,44.129,3.731,42.01.581ZM45.65,13.508a5.157,5.157,0,0,1-1.82,1.765A.729.729,0,0,1,43.124,14a3.6,3.6,0,0,0,1.3-1.249,3.54,3.54,0,0,0,.543-1.738.721.721,0,1,1,1.439.081A5.38,5.38,0,0,1,45.65,13.508Z' transform='translate(-35.275 0)' fill='currentColor' />
           </g>
         </g>
-        <g id='Rechteck_1147' data-name='Rechteck 1147' transform='translate(1678.723 744.156) rotate(-45)' fill='#ea1d25' stroke='#fff' strokeWidth='1'>
+        <g id='Rechteck_1147' data-name='Rechteck 1147' transform='translate(1678.723 744.156) rotate(-45)' fill='#ea1d25' stroke='currentColor' strokeWidth='1'>
           <rect width='19' height='3' rx='1.5' stroke='none' />
           <rect x='0.5' y='0.5' width='18' height='2' rx='1' fill='none' />
         </g>
