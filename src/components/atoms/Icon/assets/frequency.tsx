7849cf340eb543f376c51aee0aeb1f4c10cc87ec
anynines-johannchopin <jchopin@anynines.com>
2020-10-19 12:23:05 +0200

    🍱 Add calendar, arrowUpRight, euro, frequency, rocket, unit icons

---
+++ src/components/atoms/Icon/assets/frequency.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Frequency = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="19.001" height="15.962" viewBox="0 0 19.001 15.962">
+      <g id="heartbeat" transform="translate(0 -40.942)">
+        <g id="Gruppe_17030" data-name="Gruppe 17030" transform="translate(0 40.942)">
+          <path id="Pfad_15443" data-name="Pfad 15443" d="M18.241,48.544H15.707L14,44.451a.768.768,0,0,0-.752-.466.76.76,0,0,0-.683.562l-2.219,8.32L7.964,41.546a.76.76,0,0,0-.678-.6.746.746,0,0,0-.771.474L3.666,48.543H.76a.76.76,0,0,0,0,1.52H4.18a.761.761,0,0,0,.706-.478l2.108-5.269L9.517,56.3a.761.761,0,0,0,.724.6h.02a.76.76,0,0,0,.734-.563L13.459,47.1l1.04,2.5a.761.761,0,0,0,.7.467h3.04a.76.76,0,0,0,0-1.52Z" transform="translate(0 -40.942)" fill="currentColor" />
+        </g>
+      </g>
+    </svg>
+
+  )
+}
+
+export default Frequency
