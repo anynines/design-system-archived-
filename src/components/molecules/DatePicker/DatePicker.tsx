d330eb54d4988a74a513a3d40bafac2cc8296325
anynines-johannchopin <jchopin@anynines.com>
2020-10-21 14:04:52 +0200

    ✨ Handle date prop

--- src/components/molecules/DatePicker/DatePicker.tsx
+++ src/components/molecules/DatePicker/DatePicker.tsx
@@ -1,27 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Moment } from 'moment'
+import moment, { Moment } from 'moment'
 import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 import { DayPickerSingleDateController, DayPickerSingleDateControllerShape } from 'react-dates'
 
 // I N T E R F A C E S
-export type DatePickerProps = Omit<DayPickerSingleDateControllerShape, 'onDateChange' | 'renderMonthText'> & {
+export type DatePickerProps = Omit<DayPickerSingleDateControllerShape, 'onDateChange' | 'renderMonthText' | 'date'> & {
   onDateChange: (dateAsTimestamp: number) => void
+  date?: number
   className?: string
 }
 
 export const DatePicker: React.FC<DatePickerProps> = ({
   onDateChange: onDateChangeProps,
+  date,
   numberOfMonths = 1,
   className = '',
+  focused = true,
   ...rest
 }) => {
-  const onDateChange = (date: Moment | null): void => {
-    if (date !== null) {
-      onDateChangeProps(date.unix())
+  const onDateChange = (updatedDate: Moment | null): void => {
+    if (updatedDate !== null) {
+      onDateChangeProps(updatedDate.unix())
     }
+  }
+
+  const getDate = (): Moment | null => {
+    if (date) {
+      return moment.unix(date)
+    }
+
+    return null
   }
 
   return (
@@ -31,8 +42,10 @@ export const DatePicker: React.FC<DatePickerProps> = ({
 
       <DayPickerSingleDateController
         {...rest}
+        focused={focused}
         numberOfMonths={numberOfMonths}
         onDateChange={onDateChange}
+        date={getDate()}
       />
     </StyledDatePicker>
   )
@@ -44,6 +57,23 @@ const StyledDatePicker = styled.div`
   .DayPicker_transitionContainer, 
   .DayPickerNavigation_button {
     border-radius: var(--radius);
+  }
+
+  .CalendarDay__default {
+    border: 1px solid var(--color-dark-50);
+
+    &:hover {
+      background: var(--color-darker-50);
+      border: 1px double var(--color-darker);
+    }
+  }
+
+  .CalendarDay__selected, 
+  .CalendarDay__selected:active, 
+  .CalendarDay__selected:hover {
+    background: var(--color-primary);
+    border: 1px double var(--color-primary);
+    color: var(--color-white);
   }
 
   .DayPickerKeyboardShortcuts_show {
