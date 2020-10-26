69c8ffdc8f68dbb6165554e794928d14b50ee0e9
sabbir0179 <sahmed@anynines.com>
2020-10-26 13:59:44 +0100

    Add link to draggable table rows

--- src/components/organisms/Table/DraggableTableColumn.tsx
+++ src/components/organisms/Table/DraggableTableColumn.tsx
@@ -10,10 +10,13 @@ interface DraggableTableColumnProps {
   getTableColumnColor: (type: TableAccessor | null) => TableColumnCellColor | null
   getTableColumnType: (type: TableAccessor | null) => TableColumnCell | null
   getTableColumnIconType: (type: TableAccessor | null) => TableColumnIcon | null
+  isLastColumn: boolean
 }
 
 const DraggableTableColumn: React.FC<DraggableTableColumnProps> = (props) => {
-  const { cell, getTableColumnColor, getTableColumnType, getTableColumnIconType } = props
+  const {
+    cell, getTableColumnColor, getTableColumnType, getTableColumnIconType, isLastColumn = false
+  } = props
 
   let header: TableAccessor | null = cell.column.Header as TableAccessor
   if (header === undefined || Object.keys(header).length === 0) header = null
@@ -23,8 +26,9 @@ const DraggableTableColumn: React.FC<DraggableTableColumnProps> = (props) => {
   const iconType: TableColumnIcon | null = getTableColumnIconType(header)
 
   return (
-    <td role='cell'>
+    <td role='cell' className={isLastColumn ? 'table-row-link' : ''}>
       <TableCell className={className} cellType={cellType} cell={cell} iconType={iconType} />
+      {isLastColumn && <TableCell cellType='link' cell={cell} />}
     </td>
   )
 }
