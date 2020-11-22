b7a7504639e8e6fd4e394513a53c70d05f4e9c94
Sven Reiser <sreiser@anynines.com>
2020-11-23 00:25:10 +0100

    adds color component

---
+++ src/components/introduction/Color/Color.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// I N T E R F A C E
+export interface ColorProps {
+  color?: string
+  colorLabel?: string
+  textColor?: string
+  mainColor?: boolean
+}
+
+// C O M P O N E N T
+export const Color: React.FC<ColorProps> = ({
+  color = 'var(--color-black)',
+  colorLabel = 'Some black',
+  textColor = 'var(--color-white)',
+  mainColor = false
+}) => {
+  return (
+    <StyledColor
+      style={{ backgroundColor: color, color: textColor }}
+      className={mainColor && 'main-color'}
+    >
+      <span>{colorLabel}</span>
+    </StyledColor>
+  )
+}
+
+const StyledColor = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: var(--primary);
+  border-radius: var(--radius);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: var(--transition);
+  border-radius: 0;
+  
+  span {
+    font-size: 12px;
+    padding: 4px;
+    border-radius: 2px;
+    background-color: rgba(0, 0, 0, 0.2);
+    color: var(--color-white);
+  }
+
+  &.main-color {
+    span {
+      padding: 4px;
+      border-radius: 2px;
+      background-color: rgba(0, 0, 0, 0.6);
+    }
+  }
+
+  &:first-child {
+    border-radius: var(--radius) 0 0 var(--radius);
+  }
+  
+  &:last-child {
+    border-radius: 0 var(--radius) var(--radius) 0;
+  }
+`
