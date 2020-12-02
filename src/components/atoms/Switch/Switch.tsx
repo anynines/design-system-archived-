4fab5a77554e8dfc2d940da40b6ffc9a7a5450f4
Niklas Kiefer <nkiefer@anynines.com>
2020-12-02 14:48:12 +0100

    adjust styling for Switches, add type and border prop for Switch Component

--- src/components/atoms/Switch/Switch.tsx
+++ src/components/atoms/Switch/Switch.tsx
@@ -3,18 +3,25 @@ import styled from 'styled-components'
 
 // I N T E R F A C E
 export interface SwitchProps {
+  border?: boolean
   checked?: boolean
   className?: string
   label?: string
   onChange?: () => void
   style?: React.CSSProperties
+  type?: SwitchType
 }
 
+// T Y P E S
+export type SwitchType = 'slim' | 'bold'
+
 export const Switch: React.FC<SwitchProps> = ({
+  border = false,
   checked,
   className,
   onChange,
-  style
+  style,
+  type = 'slim'
 }) => {
   return (
     <StyledSwitchWrapper
@@ -22,17 +29,17 @@ export const Switch: React.FC<SwitchProps> = ({
       style={style}
     >
       <input
-        className="switch-checkbox"
+        className='switch-checkbox'
         id={`switch-new`}
         checked={checked}
         onChange={(): void => { return (onChange && onChange()) }}
-        type="checkbox"
+        type='checkbox'
       />
       <label
-        className={`switch-label ${checked && 'checked'}`}
+        className={`switch-label ${checked && 'checked'} ${type} ${border && 'border'}`}
         htmlFor={`switch-new`}
       >
-        <span className={`switch-button`} />
+        <span className={`switch-button ${type}`} />
       </label>
     </StyledSwitchWrapper>
   )
@@ -40,49 +47,82 @@ export const Switch: React.FC<SwitchProps> = ({
 
 // S T Y L E S
 const StyledSwitchWrapper = styled.div`
+  margin-right: 1rem;
+
   .switch-checkbox {
-  height: 0;
-  width: 0;
-  visibility: hidden;
-}
-
-.switch-label {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  width: 100px;
-  height: 50px;
-  background: var(--color-dark);
-  border-radius: 100px;
-  position: relative;
-  transition: background-color .2s;
-
-  &.checked{
-    background: var(--color-primary);
+    display: none;
+    height: 0;
+    width: 0;
+    visibility: hidden;
   }
-}
 
-.switch-label .switch-button {
-  content: '';
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  width: 45px;
-  height: 45px;
-  border-radius: 45px;
-  transition: 0.2s;
-  background: var(--color-white-fix);
-  box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
-}
+  .switch-label {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    width: 2.4rem;
+    background: var(--color-light-50);
+    border-radius: 3rem;
+    position: relative;
+    transition: background-color .5s;
 
-.switch-checkbox:checked + .switch-label .switch-button {
-  left: calc(100% - 2px);
-  transform: translateX(-100%);
-}
+    &.bold {
+      height: 1.2rem;
+    }
 
-.switch-label:active .switch-button {
-  width: 60px;
-}
+    &.slim {
+      height: 1rem;
+    }
+
+    &.border {
+      background: none;
+      border: 1px solid var(--color-light-50);
+    }
+
+    &.checked{
+      background: var(--color-primary);
+    }
+  }
+
+  .switch-label .switch-button {
+    content: '';
+    position: absolute;
+    border-radius: 1rem;
+    transition: 0.2s;
+    background: var(--color-white-fix);
+    box-shadow: 0 0 .4rem 0 var(--color-light-70);
+    top: 50%;
+    transform: translateY(-50%);
+
+    &.bold {
+      left: .2rem;
+      width: .8rem;
+      height: .8rem;
+    }
+
+    &.slim {
+      left: -.2rem;
+      height: 1.2rem;
+      width: 1.2rem;
+    }
+
+  }
+
+  .switch-checkbox:checked + .switch-label {
+    border: none;
+  }
+  .switch-checkbox:checked + .switch-label .switch-button {
+    transform: translate(-100%, -50%);
+
+    &.bold {
+      left: calc(100% - .2rem);
+    }
+
+    &.slim {
+      left: calc(100% + .2rem);
+    }
+  }
+
 `
 
--- src/components/atoms/Switch/stories/switch.stories.tsx
+++ src/components/atoms/Switch/stories/switch.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { action } from '@storybook/addon-actions'
 
 import { Switch, SwitchProps } from '../Switch'
 import Wrapper from '../../../_helpers/Wrapper'
@@ -13,6 +12,15 @@ export default {
         type: 'disabled'
       },
     },
+    type: {
+      control: {
+        type: 'select',
+        options: {
+          slim: 'slim',
+          bold: 'bold'
+        }
+      },
+    },
   },
 }
 
@@ -20,12 +28,65 @@ export const Basic = (args: SwitchProps) => {
   const [value, setValue] = React.useState(false)
 
   return (
-    <Wrapper bgColor='black'>
+    <Wrapper justify='flex-start' bgColor={undefined} >
       <Switch
         {...args}
         checked={value}
         onChange={() => setValue(!value)}
       />
+      { value ? 'Switch Component' : 'Press me'}
+    </Wrapper>
+  )
+}
+
+export const Type = () => {
+  const [value, setValue] = React.useState(false)
+
+  return (
+    <Wrapper align='flex-start' direction='column' bgColor='none'  >
+      <Wrapper justify='flex-start' bgColor='none'  >
+        <Switch
+          type='bold'
+          checked={value}
+          onChange={() => setValue(!value)}
+        />
+        {value ? 'Bold Switch Component' : 'Press me'}
+      </Wrapper>
+      <Wrapper justify='flex-start' bgColor='none'  >
+        <Switch
+          type='slim'
+          checked={value}
+          onChange={() => setValue(!value)}
+        />
+        {value ? 'Slim Switch Component' : 'Press me'}
+      </Wrapper>
+    </Wrapper>
+  )
+}
+
+export const Border = () => {
+  const [value, setValue] = React.useState(false)
+
+  return (
+    <Wrapper align='flex-start' direction='column' bgColor='none'  >
+      <Wrapper justify='flex-start' bgColor='none'  >
+        <Switch
+          type='bold'
+          border={true}
+          checked={value}
+          onChange={() => setValue(!value)}
+        />
+        {value ? 'Bold Border Switch Component' : 'Press me'}
+      </Wrapper>
+      <Wrapper justify='flex-start' bgColor='none'  >
+        <Switch
+          type='slim'
+          border={true}
+          checked={value}
+          onChange={() => setValue(!value)}
+        />
+        {value ? 'Slim Border Switch Component' : 'Press me'}
+      </Wrapper>
     </Wrapper>
   )
 }
