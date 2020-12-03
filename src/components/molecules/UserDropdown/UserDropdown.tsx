6545db121bed993c9fc867c99a3442e814827d6d
Sven Reiser <sreiser@anynines.com>
2020-12-03 15:16:03 +0100

    adds UserDropdown box

--- src/components/molecules/UserDropdown/UserDropdown.tsx
+++ src/components/molecules/UserDropdown/UserDropdown.tsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Avatar } from '../../atoms/Avatar/Avatar'
+import { ToggleMode } from '../../atoms/ToggleMode/ToggleMode'
 import { UserInfo } from '../UserInfo/UserInfo'
 
 export interface UserDropdownProps {
   avatarImg: string
   className?: string
+  level?: number
   name: string
-  onClick?: () => void
   role: string
   style?: React.CSSProperties
 }
 
 export const UserDropdown: React.FC<UserDropdownProps> = ({
   avatarImg,
+  children,
   className,
+  level,
   name,
-  onClick,
   role,
   style
 }) => {
+  const [active, setActive] = React.useState(false)
+  const onClick = (): void => {
+    setActive(!active)
+  }
   return (
     <StyledUserDropdown
-      className={`user-dropdown ${className}`}
-      onClick={(): void => { return (onClick && onClick()) }}
+      className={`
+        user-dropdown 
+        ${className} 
+        ${children && active && 'active'}
+      `}
       style={style}
+      onClick={(): void => { return (onClick()) }}
     >
       <Avatar
         rounded
@@ -33,6 +43,24 @@ export const UserDropdown: React.FC<UserDropdownProps> = ({
         userName={name}
       />
       <UserInfo userName={name} userGroup={role} />
+      {children && (
+        <div className='user-menu'>
+          <div className='user-menu--header'>
+            <Avatar
+              rounded
+              size='md'
+              avatar={avatarImg}
+              level={level}
+              userName={name}
+            />
+            <span className='user-menu--name'>{name}</span>
+            <span className='user-menu--role'>{role}</span>
+            <ToggleMode mode='dark' />
+            <hr />
+          </div>
+          { children }
+        </div>
+      )}
     </StyledUserDropdown>
   )
 }
@@ -41,33 +69,100 @@ const StyledUserDropdown = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  overflow: hidden;
   margin-left: 20px;
   background-color: var(--color-dark);
   max-width: 200px;
   cursor: pointer;
   padding-right: 12px;
   height: 40px;
-  border-radius: 
-    50px
-    var(--radius)
-    var(--radius)
-    50px;
+  border-radius: 50px calc(var(--radius) * 2) calc(var(--radius) * 2) 50px;
   transition: var(--transition);
+  position: relative;
 
   span {
     transition: var(--transition);
+  }
+
+  .avatar-wrapper {
+    height: 40px;
+    width: 40px;
+    margin-right: var(--space-lg);
   }
 
   .user-info {
     margin-right: 20px;
   }
 
+  .user-menu {
+    position: absolute;
+    display: none;
+    top: 60px;
+    right: 0;
+    background-color: var(--color-dark);
+    border-radius: var(--radius);
+    padding: 20px;
+    width: 260px;
+    box-shadow: var(--shadow);
+    border: var(--border);
+    flex-direction: column;
+
+    &--header {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+    }
+
+    .avatar-wrapper {
+      height: 100px;
+      width: 100px;
+      margin-bottom: var(--space-sm);
+
+      .level {
+        font-size: 14px;
+      }
+    }
+
+    &--name {
+      font-size: 18px;
+      font-weight: 900;
+    }
+
+    &--role {
+      font-size: 12px;
+      font-weight: 300;
+      opacity: 0.5;
+    }
+
+    .toggle-mode {
+      position: absolute;
+      top: 0;
+      right: 0;
+      border: var(--border);
+    }
+
+    hr {
+      margin: var(--text-sm) 0 var(--text-md);
+    }
+
+    .menu-item .wrapper {
+      background-color: var(--color-black);
+
+      &:hover {
+        background-color: var(--color-primary);
+      }
+    }
+  }
+
+
+  &.active .user-menu {
+    display: flex;
+  }
+
   &.active,
   &:hover {
     background-color: var(--color-primary);
     span {
-      opacity: 1;
       color: var(--color-white-fix);
     }
   }
--- src/components/molecules/UserDropdown/stories/UserDropdown.stories.tsx
+++ src/components/molecules/UserDropdown/stories/UserDropdown.stories.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import { action } from '@storybook/addon-actions'
 import Wrapper from '../../../_helpers/Wrapper'
 import { UserDropdown, UserDropdownProps } from '../UserDropdown'
+import { Menu } from '../../../organisms/Menu/Menu'
+import { MenuItem } from '../../MenuItem/MenuItem'
+
+const avatarUrl = 'https://images.pexels.com/photos/936559/pexels-photo-936559.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
 
 export default {
   title: '🌱 Molecules/UserDropdown',
@@ -17,7 +22,7 @@ export default {
     },
     avatarImg: {
       control: { type: 'text' },
-      defaultValue: 'https://images.pexels.com/photos/936559/pexels-photo-936559.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+      defaultValue: avatarUrl
     }
   }
 }
@@ -25,5 +30,22 @@ export default {
 export const Basic = (args: UserDropdownProps) => (
   <Wrapper bgColor='black'>
     <UserDropdown {...args} />
+  </Wrapper>
+)
+
+export const WithDropdown = () => (
+  <Wrapper bgColor='black'>
+    <UserDropdown
+      name='Johanna Doe'
+      role='Contributor'
+      level={42}
+      avatarImg={avatarUrl}
+    >
+      <Menu vertical>
+        <MenuItem icon={'menu'} vertical onClick={action('label clicked')}>
+          <>Icon and Label</>
+        </MenuItem>
+      </Menu>
+    </UserDropdown>
   </Wrapper>
 )
