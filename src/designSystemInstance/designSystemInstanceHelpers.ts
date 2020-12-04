8c69506af223029de9183bc1b7c997ffabf00f8f
Sven Reiser <sreiser@anynines.com>
2020-12-04 11:27:13 +0100

    removes defaultBrowserMode and fixes setMode

--- src/designSystemInstance/designSystemInstanceHelpers.ts
+++ src/designSystemInstance/designSystemInstanceHelpers.ts
@@ -1,20 +1,14 @@
 import { LocalStorage } from '../helpers/localStorage'
 import { Theme, Mode } from './types/types'
 
-export const getTheme = <T extends Theme>(
-  theme: T
-): T => {
+export const getTheme = <T extends Theme>(theme: T): T => {
   return theme
 }
 
-export const getMode = (
-  mode: Mode,
-  storagePrefix: string,
-  useLocalStorage: boolean
-): Mode => {
+export const getMode = (mode: Mode, storagePrefix: string): Mode => {
   const modeStoredInLocalStorage = LocalStorage.getItem(`${storagePrefix}mode`)
 
-  if (useLocalStorage && modeStoredInLocalStorage !== null) {
+  if (modeStoredInLocalStorage !== null) {
     return modeStoredInLocalStorage as Mode
   }
 
@@ -31,8 +25,8 @@ export const getToggledTheme = <T extends Theme>(theme: T): T => {
         white: theme.globals.colors.black,
         light: theme.globals.colors.dark,
         dark: theme.globals.colors.light,
-        black: theme.globals.colors.white
-      }
-    }
+        black: theme.globals.colors.white,
+      },
+    },
   }
 }
