e72bf761b15bd1c42de135ea364a567bdc4f4a32
Niklas Kiefer <nkiefer@anynines.com>
2020-12-08 15:32:08 +0100

    update changelog

--- src/components/introduction/ChangeLog/changeLog.stories.tsx
+++ src/components/introduction/ChangeLog/changeLog.stories.tsx
@@ -5,6 +5,7 @@ import { ChangeLog } from './ChangeLog'
 
 // Changelogs
 import v0_1_37 from './logs/0.1.37.json'
+import v0_1_38 from './logs/0.1.38.json'
 
 export default {
   title: '👋 Get started/Changelog',
@@ -14,6 +15,12 @@ export const Welcome = () => (
   <Section bgColor='transparent'>
     <Container style={{ marginTop: 50, width: 800 }}>
       <h2 style={{ marginBottom: 30 }}>Changelog</h2>
+      <ChangeLog
+        version='0.1.38'
+        date='08. December 2020'
+        isPublished={false}
+        changelog={v0_1_38}
+      />
       <ChangeLog
         version='0.1.37'
         date='08. December 2020'
