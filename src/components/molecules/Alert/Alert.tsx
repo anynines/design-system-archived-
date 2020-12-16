1e71ade6cd3736a4166b5892486c4d5468b61965
Niklas Kiefer <nkiefer@anynines.com>
2020-12-16 10:02:30 +0100

    fix linter issues

--- src/components/molecules/Alert/Alert.tsx
+++ src/components/molecules/Alert/Alert.tsx
@@ -42,7 +42,6 @@ export const Alert: React.FC<AlertProps> = ({
 
     return (): void => {
       if (withTimeout) {
-        console.log('timer closed')
         clearTimeout(timer)
       }
     }
