3a159123a605ca17b8e467708d38449c16da6a89
Moritz Ney <mney@anynines.com>
2021-01-12 13:59:51 +0100

    add a new usage story for animations and update the overview page with more variants

--- src/components/introduction/Animation/stories/animation.stories.tsx
+++ src/components/introduction/Animation/stories/animation.stories.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 import Wrapper from '../../../_helpers/Wrapper'
 import { Animation } from '../Animation'
+import { Code } from '../../../atoms/Code/Code'
 
 export default {
   title: '👋 Get started/ Animation',
@@ -16,6 +17,17 @@ export default {
   }
 }
 
+const codeSnippetHover = (`.container:hover {
+  animation: bounce;
+  animation-duration: 0.5s;
+}
+`)
+
+const codeSnippetHtml = (`<div class="animate__animated animate__bounce">
+  Content
+</div>
+`)
+
 export const Overview = () => (
   <Wrapper align='flex-start' direction='column'>
     <section>
@@ -23,7 +35,9 @@ export const Overview = () => (
       <p>
         We use animate.css for all animation purposes.
         You can select from a lot of different animations where some of them are previewed in this component.
-        For more animations and info about animate.css you can visit: <a className='link' href="https://animate.style/" target="_blank">animate.css</a>
+      </p>
+      <p>
+        <strong>You need to click on the boxes to trigger the appropriate animation.</strong>
       </p>
     </section>
     <section style={{ marginTop: 30, marginBottom: 30 }}>
@@ -94,6 +108,41 @@ export const Overview = () => (
     </section>
 
     <section style={{ marginTop: 30, marginBottom: 30 }}>
+      <h5 style={{ marginBottom: 10 }}>Misc</h5>
+      <StyledAnimationList>
+        <Animation type='flash'>
+          <StyledAnimatedDiv />
+          <span>flash</span>
+        </Animation>
+
+        <Animation type='heartBeat'>
+          <StyledAnimatedDiv />
+          <span>heartBeat</span>
+        </Animation>
+
+        <Animation type='jello'>
+          <StyledAnimatedDiv />
+          <span>jello</span>
+        </Animation>
+
+        <Animation type='pulse'>
+          <StyledAnimatedDiv />
+          <span>pulse</span>
+        </Animation>
+
+        <Animation type='shakeX'>
+          <StyledAnimatedDiv />
+          <span>shakeX</span>
+        </Animation>
+
+        <Animation type='shakeY'>
+          <StyledAnimatedDiv />
+          <span>shakeY</span>
+        </Animation>
+      </StyledAnimationList>
+    </section>
+
+    <section style={{ marginTop: 30, marginBottom: 30 }}>
       <h5 style={{ marginBottom: 10 }}>Rotate In/Out</h5>
       <p>All rotateIn animations can also be used as rotateOut.</p>
       <StyledAnimationList>
@@ -120,6 +169,32 @@ export const Overview = () => (
         <Animation type='rotateInUpRight'>
           <StyledAnimatedDiv />
           <span>rotateInUpRight</span>
+        </Animation>
+      </StyledAnimationList>
+    </section>
+
+    <section style={{ marginTop: 30, marginBottom: 30 }}>
+      <h5 style={{ marginBottom: 10 }}>Slide In/Out</h5>
+      <p>All slideIn animations can also be used as slideOut.</p>
+      <StyledAnimationList>
+        <Animation type='slideInDown'>
+          <StyledAnimatedDiv />
+          <span>slideInDown</span>
+        </Animation>
+
+        <Animation type='slideInLeft'>
+          <StyledAnimatedDiv />
+          <span>slideInLeft</span>
+        </Animation>
+
+        <Animation type='slideInRight'>
+          <StyledAnimatedDiv />
+          <span>slideInRight</span>
+        </Animation>
+
+        <Animation type='slideInUp'>
+          <StyledAnimatedDiv />
+          <span>slideInUp</span>
         </Animation>
       </StyledAnimationList>
     </section>
@@ -153,6 +228,45 @@ export const Overview = () => (
           <span>zoomInUpRight</span>
         </Animation>
       </StyledAnimationList>
+    </section>
+  </Wrapper >
+)
+
+export const Usage = () => (
+  <Wrapper align='flex-start' direction='column'>
+    <h2>Usage</h2>
+    <p>
+      You can simple use the animations via css or by passing the appropriate css classes.
+      <br />
+      If you need more animations or more information about animate.css you can visit: <a className='link' href="https://animate.style/" target="_blank">animate.css</a>
+    </p>
+    <section style={{ marginTop: 20, marginBottom: 20 }}>
+      <h5 style={{ marginBottom: 10 }}>Hover via CSS</h5>
+      <Code language='css'>
+        <pre>
+          <code>
+            {codeSnippetHover}
+          </code>
+        </pre>
+      </Code>
+    </section>
+
+    <section style={{ marginTop: 20, marginBottom: 20 }}>
+      <h5 style={{ marginBottom: 10 }}>Using CSS classes</h5>
+      <Code language='html'>
+        <pre>
+          <code>
+            {codeSnippetHtml}
+          </code>
+        </pre>
+      </Code>
+    </section>
+
+    <section style={{ marginTop: 20, marginBottom: 20 }}>
+      <h5 style={{ marginBottom: 10 }}>Events</h5>
+      <p>You can also use js events (e.g. onClick) to render these classes.
+        <br />
+        You can see the result for onClick events at the overview page.</p>
     </section>
   </Wrapper>
 )
