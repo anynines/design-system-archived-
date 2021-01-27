c949daa44e303e4264678d8df9830876a7039576
anynines-johannchopin <jchopin@anynines.com>
2021-01-27 15:32:29 +0100

    🏷️ DefaultComponentProps helper

--- src/types/react.ts
+++ src/types/react.ts
@@ -2,3 +2,5 @@ import { AriaAttributes, DOMAttributes } from 'react'
 
 export type ReactHtmlElementProps<T extends AriaAttributes | DOMAttributes<T>>
   = React.DetailedHTMLProps<React.HTMLAttributes<T>, T>
+
+export type DefaultComponentProps = Pick<ReactHtmlElementProps<HTMLDivElement>, 'className' | 'style'>
