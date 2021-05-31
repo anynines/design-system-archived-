5e9fab08721fe9796d2fc51ee32b22f5e1396fd5
anynines-johannchopin <jchopin@anynines.com>
2021-05-31 13:42:03 +0200

    use typescript plugin instead of webpack for sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,24 +2,14 @@ const path = require('path')
 
 module.exports = {
   stories: [path.resolve(__dirname, "../src/**/*.stories.(ts|tsx|js|jsx|mdx)")],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(tsx|ts)?$/,
-      include: path.resolve(__dirname, "../src"),
-      use: [
-        require.resolve("ts-loader"),
-        {
-          loader: require.resolve("react-docgen-typescript-loader"),
-          options: {
-            // Provide the path to your tsconfig.json so that your stories can
-            // display types from outside each individual story.
-            tsconfigPath: path.resolve(__dirname, "./../tsconfig.json"),
-          },
-        },
-      ],
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
-    return config
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
   },
   addons: [
     '@storybook/addon-links',
