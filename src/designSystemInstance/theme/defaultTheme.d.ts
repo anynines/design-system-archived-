2bf40cc50c55fd7b78486e37f4b2905465132be9
sabbir0179 <sahmed@anynines.com>
2021-06-16 21:46:46 +0200

    make new build

---
+++ src/designSystemInstance/theme/defaultTheme.d.ts
@@ -0,0 +1,2 @@
+import { Theme } from '../types/types';
+export declare const defaultTheme: Theme;
---
+++ src/designSystemInstance/types/types.d.ts
@@ -0,0 +1,79 @@
+/// <reference types="react" />
+export declare type Mode = 'dark' | 'light';
+export interface Globals {
+    font: {
+        name: string;
+        light: string;
+        regular: string;
+        bold: string;
+    };
+    colors: {
+        primary: string;
+        notice: string;
+        success: string;
+        warning: string;
+        error: string;
+        whiteFix: string;
+        white: string;
+        light: string;
+        dark: string;
+        black: string;
+        pageBgColor: string;
+        layoutBgColor: string;
+        boxBgColor: string;
+        elementBgColor: string;
+        textColor: string;
+    };
+    radius: string;
+    contentMaxWidth: string;
+    border: string;
+    shadow: string;
+    transition: string;
+    logos: {
+        vertical: {
+            dark: string;
+            light: string;
+        };
+        horizontal: {
+            dark: string;
+            light: string;
+        };
+    };
+    spacing: {
+        baseUnit: string;
+        fixedUnit: string;
+    };
+    typography: {
+        baseUnit: string;
+        headingLineHeight: string;
+        bodyLineHeight: string;
+    };
+}
+export interface Theme {
+    name: string;
+    globals: Globals;
+}
+export declare type PartialTheme = RecursivePartial<Theme>;
+declare type RecursivePartial<T> = {
+    [P in keyof T]?: T[P] extends (infer U)[] ? RecursivePartial<U>[] : T[P] extends object ? RecursivePartial<T[P]> : T[P];
+};
+export interface ThemeContextParam<T extends Theme> {
+    theme: T;
+    setTheme: (newTheme: T) => void;
+    resetTheme: () => void;
+    mode: Mode;
+    setMode: (newMode: Mode) => void;
+}
+export interface DesignSystemInstanceProps<T> {
+    theme?: T;
+    persistantModeStorage?: boolean;
+    mode?: Mode;
+    useFavicon?: boolean;
+    autoDetectPreferenceMode?: boolean;
+    storagePrefix?: string;
+}
+export interface DesignSystemContextElements<T> {
+    ThemeContext: React.Context<ThemeContextParam<T & Theme>>;
+    DesignSystemInstance: React.FC<DesignSystemInstanceProps<T>>;
+}
+export {};
