7192d52bcd04e5ae6b8844bd8bc7535843452704
sabbir0179 <sahmed@anynines.com>
2021-06-20 20:15:07 +0200

    Update card story

--- src/components/molecules/Card/stories/card.stories.tsx
+++ src/components/molecules/Card/stories/card.stories.tsx
@@ -109,6 +109,18 @@ export default {
         }
       },
       defaultValue: 'image'
+    },
+    tagClassName: {
+      control: {
+        type: 'string'
+      },
+      defaultValue: ''
+    },
+    contentTypeIconClassName: {
+      control: {
+        type: 'string'
+      },
+      defaultValue: ''
     }
   }
 }
@@ -224,8 +236,8 @@ export const CustomTitles = () => {
             imageUrl='https://llandscapes-10674.kxcdn.com/wp-content/uploads/2019/07/lighting.jpg'
             linkUrl='https://youtube.com'
             tag='tag'
-            tagClassName={'tag-styles'}
-            contentTypeIconClassName={'content-type-icon-styles'}
+            tagClassName='tag-styles'
+            contentTypeIconClassName='content-type-icon-styles'
             contentTypeIcon='camera'
           />
           <Card.Title>
