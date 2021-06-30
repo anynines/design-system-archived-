a1477d13ca29c34e88be4179ff577842bae29c2a
sabbir0179 <61695002+sabbir0179@users.noreply.github.com>
2021-06-30 09:48:25 +0000

    Deploying to gh-pages from  @ 4177b0d689eaf3f5ac65f6901bfbf56a2b458149 🚀

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.050d0ebf548fdd02d9a45ab3428643a5.js"
+  "precache-manifest.699fe110b125f2e849482850f7338299.js"
 );
 
 self.addEventListener('message', (event) => {
