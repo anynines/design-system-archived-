2c9a71b0813a67d681cbb7909e9b4e3020217147
sabbir0179 <sahmed@anynines.com>
2021-07-08 04:44:32 +0200

    Update changelog for v0.8.2 changes

--- src/components/introduction/ChangeLog/changeLog.stories.tsx
+++ src/components/introduction/ChangeLog/changeLog.stories.tsx
@@ -24,6 +24,7 @@ import v0_7_8 from './logs/0.7.8.json'
 import v0_7_9 from './logs/0.7.9.json'
 import v0_8_0 from './logs/0.8.0.json'
 import v0_8_1 from './logs/0.8.1.json'
+import v0_8_2 from './logs/0.8.2.json'
 
 export default {
   title: '👋 Get started/Changelog',
@@ -33,6 +34,12 @@ export const Welcome = () => (
   <Section bgColor='transparent'>
     <Container style={{ marginTop: 50, width: 800 }}>
       <h2 style={{ marginBottom: 30 }}>Changelog</h2>
+      <ChangeLog
+        version='0.8.2'
+        date='8. July 2021'
+        isPublished={true}
+        changelog={v0_8_2}
+      />
       <ChangeLog
         version='0.8.1'
         date='5. July 2021'
